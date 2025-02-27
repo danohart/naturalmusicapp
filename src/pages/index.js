f6cf83bf3786231cda371d2dc1a05f154a7155a2
Daniel Hart <me@danielhart.co>
2025-02-27 12:14:43 -0600

    Update homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,38 @@
-import { Container, Row, Col, Spinner } from "react-bootstrap";
+import React from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { useRouter } from "next/router";
 import Head from "next/head";
-import Link from "next/link";
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleRedirect = () => {
+    router.push("/music/1");
+  };
+
   return (
-    <Container>
+    <Container className='vh-100 d-flex flex-column justify-content-center align-items-center'>
       <Head>
-        <title>Church Setlist // Natural Music App</title>
+        <title>Natural Music App</title>
       </Head>
+
       <Row>
-        <Col className='mt-4'>
-          <h1>Church Setlist</h1>
-          <hr />
-        </Col>
-      </Row>
-      <Row>
-        <Col className='mt-4'>
-          <Link href='/music/1'>
-            <h2>Go to Music</h2>
-          </Link>
+        <Col className='text-center'>
+          <h5 className='text-muted fw-light mb-2'>Welcome to</h5>
+          <h1 className='display-3 fw-bold mb-4'>Natural Music App</h1>
+
+          <p className='lead mb-5'>
+            Find your favorite church music right here.
+          </p>
+
+          <Button
+            size='lg'
+            variant='primary'
+            onClick={handleRedirect}
+            className='px-5 py-3 shadow-sm'
+          >
+            View Church Setlist
+          </Button>
         </Col>
       </Row>
     </Container>
