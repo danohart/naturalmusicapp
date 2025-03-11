9be58e7aa61232cbe70976687be1ec293ee9aec1
Daniel Hart <me@danielhart.co>
2025-03-11 17:05:46 -0500

    Update api call

--- src/pages/api/music/[page].js
+++ src/pages/api/music/[page].js
@@ -1,5 +1,4 @@
 import { getPosts } from "../../../lib/wordpress";
-import { extractAudioFiles } from "../../../lib/extractAudioFiles";
 
 export default async function handler(req, res) {
   const { page, perPage } = req.query;
@@ -11,22 +10,10 @@ export default async function handler(req, res) {
       `&_embed`
   );
 
-  const processedResponse = postsResponse.map((post) => {
-    return {
-      id: post.id,
-      title: post.title.rendered,
-      slug: post.slug,
-      date: post.date,
-      audioFiles: extractAudioFiles(post.content.rendered),
-      featuredImage:
-        post._embedded?.["wp:featuredmedia"]?.[0]?.source_url || null,
-    };
-  });
-
   res.setHeader(
     "Cache-Control",
     "public, s-maxage=86400, stale-while-revalidate=43200"
   );
 
-  return res.status(200).json(processedResponse);
+  return res.status(200).json(postsResponse);
 }
